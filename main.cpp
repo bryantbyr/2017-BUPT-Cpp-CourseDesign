beb16cdccaa681f474fb0c6110b6aaa38dc83b1e
bryant_byr <qirui@bupt.edu.cn>
2017-04-09 12:30:07 +0800

    first commit on 20170409

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class User{
+private:
+    string name;
+public:
+    string getName();
+};
+
+class Player:public User{
+
+};
+
+class TestBuilder:public User{
+
+};
+
+
+int main
+{
+
+    return 0;
+}
